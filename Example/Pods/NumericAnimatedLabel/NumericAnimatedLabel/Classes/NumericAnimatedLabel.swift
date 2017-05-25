fe81de39e6b8cd1d420aa6b0fc36f2732ae75cc0
Javal Nanda <javalnanda@gmail.com>
2017-05-25 17:18:34 +0530

    pod update

--- Example/Pods/NumericAnimatedLabel/NumericAnimatedLabel/Classes/NumericAnimatedLabel.swift
+++ Example/Pods/NumericAnimatedLabel/NumericAnimatedLabel/Classes/NumericAnimatedLabel.swift
@@ -16,12 +16,12 @@ public class NumericAnimatedLabel: UIView {
      // Drawing code
      }
      */
-
+    
     var animationTimer: Timer?
     var targetValue: Double = 0.0
     var currentValue: Double = 0.0
     var stepSize: Double = 0.0
-
+    
     var label: UILabel!
     open var textFont: UIFont = UIFont.systemFont(ofSize: 16.0)
     open var textAlignment: NSTextAlignment = NSTextAlignment.center {
@@ -29,28 +29,28 @@ public class NumericAnimatedLabel: UIView {
             label.textAlignment = textAlignment
         }
     }
-
+    
     @IBInspectable open var labelTextColor: UIColor = UIColor.black {
         didSet {
             label.textColor = labelTextColor
         }
     }
-
+    
     open var formatString: String = "%.2f"
-
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
         updateView()
     }
-
+    
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         updateView()
     }
-
+    
     override public func layoutSubviews() {
         super.layoutSubviews()
-
+        
         label.textColor = labelTextColor
         label.textAlignment = textAlignment
         label.font = textFont
@@ -59,32 +59,32 @@ public class NumericAnimatedLabel: UIView {
         self.addConstraint(label, toView: self, attribute: .leading, constant: 0.0)
         self.addConstraint(label, toView: self, attribute: .bottom, constant: 0.0)
         self.addConstraint(label, toView: self, attribute: .trailing, constant: 0.0)
-
+        
     }
-
+    
     func addConstraint(_ newView: UIView, toView: UIView, attribute: NSLayoutAttribute, constant: CGFloat) {
         let constraint =  NSLayoutConstraint(item: newView, attribute: attribute, relatedBy: .equal, toItem: toView, attribute: attribute, multiplier: 1.0, constant: constant)
         toView.addConstraint(constraint)
     }
-
+    
     func updateView() {
         self.clipsToBounds = true
         label = UILabel(frame: self.frame)
         self.addSubview(label)
     }
-
+    
     public func setValue(value: Double) {
         if animationTimer != nil {
             animationTimer?.invalidate()
         }
-
+        
         targetValue = value
         stepSize = (targetValue - currentValue) / 20.0
-
+        
         animationTimer = Timer.scheduledTimer(timeInterval: (1/40.0), target: self, selector: #selector(onAnimationTimer), userInfo: nil, repeats: true)
-
+        
     }
-
+    
     func onAnimationTimer() {
         currentValue += stepSize
         updateCurrentValue(value: currentValue)
@@ -110,9 +110,9 @@ public class NumericAnimatedLabel: UIView {
             }
         }
     }
-
+    
     func updateCurrentValue(value: Double) {
         self.label.text = String(format: self.formatString, value)
     }
-
+    
 }
